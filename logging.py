5880565a8ca0da4038b68bbff8d08b3ea16648f7
JonasErtl <nilshofmeier@gmail.com>
2023-05-01 18:23:47 +0200

    Added Client Protocol

--- logging.py
+++ logging.py
@@ -2,7 +2,9 @@ import time
 import board
 import adafruit_mpu6050
 import os 
+import client
 
+#I2C Setup 
 i2c = board.I2C()
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
@@ -11,7 +13,9 @@ while True:
     x = round(mpu.gyro[0] + 0.01986460150976955 ,2)
     y = round(mpu.gyro[1] + 0.49588791280790145 ,2)
     z = round(mpu.gyro[2] + 0.004100478185762012,2)
+    gyro_data = (x,y,z)
     print(f"Gyro: X {x}, Y {y}, Z {z}")
+    client.send(gyro_data)
     time.sleep(0.1)
     os.system('clear')
 
--- visual/target/debug/deps/visual-728419ce1b59258c.d
+++
@@ -1,5 +0,0 @@
-/home/jonas/Desktop/Dev/Engineering/MOTION/visual/target/debug/deps/visual-728419ce1b59258c.rmeta: src/main.rs
-
-/home/jonas/Desktop/Dev/Engineering/MOTION/visual/target/debug/deps/visual-728419ce1b59258c.d: src/main.rs
-
-src/main.rs:
