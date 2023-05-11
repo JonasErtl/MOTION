656d38ebe0ec0cf0606e56a52a40c6b25240534a
JonasErtl <nilshofmeier@gmail.com>
2023-05-11 22:20:34 +0200

    started working on data transfer

--- recieve.py
+++ recieve.py
@@ -2,4 +2,7 @@ from time import sleep
 
 while True:
     file1 = open("file.txt", "r")
-    print(file1)
+    lis = file1.read()
+    a = lis.strip().split(',')
+    print("The X Value: %s The Y Value: %s The Z Value: %s", {a[0], a[1], a[2]})
+    sleep(0.1)
